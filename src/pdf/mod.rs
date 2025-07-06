2bb4de726be7e017e9cb2646021e1b645ca0eb34
kartik6717 <pithavakeval66@gmail.com>
2025-07-06 12:53:23 +0530

    Update mod.rs

--- src/pdf/mod.rs
+++ src/pdf/mod.rs
@@ -4,12 +4,29 @@ mod parser;
 mod objects;
 mod xref;
 mod dict;
+mod stream;
+mod filter;
 
 pub use parser::PDFParser;
-use objects::{PDFObject, PDFObjectType};
+pub use dict::Dictionary;
+use objects::{PDFObject, ObjectType};
 use xref::XRefTable;
-use dict::Dictionary;
+use stream::Stream;
+use filter::Filter;
 
 use std::collections::HashMap;
 use std::io::{self, Read, Write, Seek};
-use crate::error::PDFCryptoError;
+use log::{debug, trace, warn};
+use crate::error::{PDFCryptoError, PDFCryptoResult};
+
+/// Common traits for PDF objects
+pub(crate) trait PDFObjectCommon {
+    /// Get object type
+    fn get_type(&self) -> ObjectType;
+    
+    /// Write object to output
+    fn write_to(&self, output: &mut Vec<u8>);
+    
+    /// Clone object
+    fn clone_object(&self) -> Box<dyn PDFObjectCommon>;
+}
